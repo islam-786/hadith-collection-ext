6b4deeb200e724b4aaa0c6575ea4bbb8506b4680
AxeemHaider <axeemhaider@gmail.com>
2020-06-17 23:58:53 +0500

    new way to fetch data by child nodes

--- hadith_scripts/bukhari.js
+++ hadith_scripts/bukhari.js
@@ -1,25 +1,55 @@
 const selector = {
   bookName: ".book_page_english_name",
-  hadithCount: ".AllHadith .actualHadithContainer",
-  chapterName: ".AllHadith .chapter .englishchapter",
-  narratedBy: ".AllHadith .actualHadithContainer .hadith_narrated",
-  hadithContent: ".AllHadith .actualHadithContainer .text_details",
+  chapterName: "englishchapter",
+  narratedBy: ["englishcontainer", "english_hadith_full", "hadith_narrated"],
+  hadithContent: ["englishcontainer", "english_hadith_full", "text_details"],
+  hadithCollection: ".AllHadith>div",
+};
+
+const classes = {
+  chapter: "chapter",
+  hadith: "actualHadithContainer",
 };
 
 function collectBukhariHadith() {
   const bookName = fetchContent(selector.bookName);
   const data = [];
+  let hadith = {};
+  let hadithCount = 1;
+  let currentChapter = "";
 
-  for (var i = 0; i < countCollection(selector.hadithCount); i++) {
-    let hadith = {};
-    hadith.book = bookName;
-    hadith.NumberInBook = i + 1;
-    hadith.chapter = getChapterName(selector.chapterName, i);
-    hadith.narratedBy = fetchNarratedBy(selector.narratedBy, i);
-    hadith.text = fetchHadithContent(selector.hadithContent, i);
+  hadithCollection(selector.hadithCollection).each(function (index) {
+    let node = this;
 
-    data.push(hadith);
-  }
+    if (checkClass(node, classes.chapter)) {
+      currentChapter = getTextFromChildNode(
+        node,
+        selector.chapterName,
+        filterChapterName
+      );
+    }
+
+    if (checkClass(node, classes.hadith)) {
+      hadith.book = bookName;
+      hadith.NumberInBook = hadithCount;
+      hadith.chapter = currentChapter;
+      hadith.narratedBy = getTextFromChildNode(
+        node,
+        selector.narratedBy,
+        filterNarratedBy
+      );
+      hadith.text = getTextFromChildNode(
+        node,
+        selector.hadithContent,
+        filterHadithContent
+      );
+
+      data.push(hadith);
+
+      hadithCount++;
+      hadith = {};
+    }
+  });
 
   console.log(data);
 }
--- hadith_scripts/common.js
+++ hadith_scripts/common.js
@@ -27,23 +27,65 @@ function fetchContent(selector) {
   return removeWhiteSpaces(text);
 }
 
-function countCollection(selector) {
-  return $(selector).length;
+function filterChapterName(text) {
+  return removeWhiteSpaces(text.replace("Chapter: ", ""));
 }
 
-function getChapterName(selector, index) {
-  const chapter = $(selector)[index];
-  return removeWhiteSpaces(chapter.textContent.replace("Chapter: ", ""));
+function filterNarratedBy(text) {
+  let narratedBy = text.replace("Narrated '", "");
+  narratedBy = narratedBy.replace(":", "");
+  return removeWhiteSpaces(narratedBy);
 }
 
-function fetchNarratedBy(selector, index) {
-  const narratedBy = $(selector)[index];
-  let text = narratedBy.textContent.replace("Narrated '", "");
-  text = text.replace(":", "");
+function filterHadithContent(text) {
   return removeWhiteSpaces(text);
 }
 
-function fetchHadithContent(selector, index) {
-  const hadith = $(selector)[index];
-  return removeWhiteSpaces(hadith.textContent);
+function hadithCollection(selector) {
+  return $(selector);
+}
+
+function checkClass(node, name) {
+  if (node.className == name) {
+    return true;
+  }
+
+  return false;
+}
+
+function getChildNode(node, selector) {
+  for (var i = 0; i < node.childNodes.length; i++) {
+    let child = node.childNodes[i];
+    if (child.className == selector) {
+      return child;
+    }
+  }
+}
+
+function getChildeNodeContent(node, selector, filter = false) {
+  for (var i = 0; i < node.childNodes.length; i++) {
+    let child = node.childNodes[i];
+    if (child.className == selector) {
+      if (filter) {
+        return filter(child.innerText);
+      }
+
+      return removeWhiteSpaces(child.innerText);
+    }
+  }
+}
+
+function getTextFromChildNode(node, selector, filter = false) {
+  if ($.isArray(selector)) {
+    let child = node;
+    for (var i = 0; i < selector.length; i++) {
+      if (i == selector.length - 1) {
+        return getChildeNodeContent(child, selector[i], filter);
+      } else {
+        child = getChildNode(child, selector[i]);
+      }
+    }
+  } else {
+    return getChildeNodeContent(node, selector, filter);
+  }
 }
